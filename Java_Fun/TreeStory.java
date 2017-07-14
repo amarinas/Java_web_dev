ba7ee29215fb7d8c99962865995458e3aa4ae516
amarinas <ali.marinas@icloud.com>
2017-07-14 13:07:26 -0700

    added while loop for specific noun and age limit

--- Java_Fun/TreeStory.java
+++ Java_Fun/TreeStory.java
@@ -13,11 +13,29 @@ public class TreeStory{
      verb - an Action
      adjective - a description used to modify or describe a noun
   */
-
+    String ageAsString = console.readLine("How old are you? ");
+    int age = Integer.parseInt(ageAsString);
+    if (age < 13){
+      console.printf("sorry you be at least 13 to use this program. \n");
+      System.exit(0);
+    }
     String name = console.readLine("Enter a name:  ");
     String adjective = console.readLine("Enter an adjective:  ");
-    String noun = console.readLine("Enter a noun:  ");
-    String adverb = console.readLine("Enter an adverb:  ");
+    String noun;
+    boolean isInvalidWord;
+    do {
+        noun = console.readLine("Enter a noun:  ");
+        isInvalidWord = (noun.equalsIgnoreCase("dork") ||
+              noun.equalsIgnoreCase("jerk"));
+
+        if(isInvalidWord){
+          console.printf("be nice.. try again! \n\n");
+
+        }
+
+    }while(isInvalidWord);
+
+    String adverb = console.readLine("Enter an adverb: ");
     String verb = console.readLine("Enter a verb ending with -ing:  ");
 
     console.printf("Your TreeStory:\n -----------\n");
