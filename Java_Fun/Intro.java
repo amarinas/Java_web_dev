b2424db39ff1f4eeb6a98dd4de5e16eda04055c0
amarinas <ali.marinas@icloud.com>
2017-07-14 11:37:50 -0700

    added input from user

--- Java_Fun/Intro.java
+++ Java_Fun/Intro.java
@@ -4,7 +4,12 @@ public class Intro{
 
   public static void main(String[]args){
     Console console = System.console();
+    String bye = "goodbye";
     console.printf("hello, my name is ali\n");
+    console.printf("%s see you later\n", bye);
 
+    String yourName = console.readLine("What is your name?  ");
+
+    console.printf("nice to meet you %s \n", yourName);
   }
 }
