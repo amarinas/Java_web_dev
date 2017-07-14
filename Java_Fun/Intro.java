a0bf6c0c707f67cae79607aefa3680d52cdda77f
amarinas <ali.marinas@icloud.com>
2017-07-14 11:23:12 -0700

    initial

---
+++ Java_Fun/Intro.java
@@ -0,0 +1,10 @@
+import java.io.Console;
+
+public class Intro{
+
+  public static void main(String[]args){
+    Console console = System.console();
+    console.printf("hello, my name is ali\n");
+
+  }
+}
