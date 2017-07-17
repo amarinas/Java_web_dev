fb21763294dec9d35ec54e45aab772822335ff0c
amarinas <ali.marinas@icloud.com>
2017-07-17 12:25:06 -0700

    added a prompter for answer

--- Java_Fun/hangman_game/Hangman.java
+++ Java_Fun/hangman_game/Hangman.java
@@ -1,8 +1,13 @@
 public class Hangman{
 
   public static void main(String[] args){
+    if (args.length ==0){
+      System.out.println("Usage: java hangmean <answer>");
+      System.err.println("answer is required");
+      System.exit(1);
 
-    Game game = new Game("treehouse");
+    }
+    Game game = new Game(args[0]);
     Promter prompter = new Promter(game);
     while(game.getRemainingTries()> 0 && !game.isWon()){
 
